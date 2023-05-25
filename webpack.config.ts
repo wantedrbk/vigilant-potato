65e016697d046cef31f760a3ee45a5cfc52350a9
KDV <wexwexwex1@gmail.com>
2023-05-25 23:18:39 +0800

    second part, added config decomposition and some conf options

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,35 +1,17 @@
-import path from 'path';
 import webpack from 'webpack';
-import HTMLWebpackPlugin from "html-webpack-plugin";
+import {buildWebpackConfig} from "./config/build/buildWebpackConfig";
+import {BuildPaths} from "./config/build/types/config";
+import path from "path";
 
-const config: webpack.Configuration = {
-    mode: 'development',
-    entry: path.resolve(__dirname, 'src', 'index.ts'),
-    module: {
-      rules: [
-        {
-          test: /\.tsx?$/,
-          use: 'ts-loader',
-          exclude: /node_modules/,
-        },
-      ],
-    },
-    resolve: {
-      extensions: ['.tsx', '.ts', '.js'],
-    },
-    output: {
-      filename: '[name].[contenthash].js',
-      path: path.resolve(__dirname, 'build'),
-      clean: true,
-    },
-    plugins: [
-      new HTMLWebpackPlugin({
-        template: path.resolve(__dirname, 'public', 'index.html'),
-        title: 'My App',
-        filename: 'index.html'
-      }),
-      new webpack.ProgressPlugin()
-    ],
+const paths: BuildPaths = {
+  entry: path.resolve(__dirname, 'src', 'index.ts'),
+  build: path.resolve(__dirname, 'public', 'index.html'),
+  html: path.resolve(__dirname, 'public', 'index.html')
 }
+
+const config: webpack.Configuration = buildWebpackConfig({
+  mode: 'development',
+  paths
+})
 
 export default config
