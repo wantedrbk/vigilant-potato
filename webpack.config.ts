88b0f214b5333f4beecc70f21724e23c042fc08d
KDV <wexwexwex1@gmail.com>
2023-05-25 22:02:37 +0800

    first part , last changes

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,34 +1,35 @@
-import path from 'path'
-import webpack from 'webpack'
-import HtmlWebpackPlugin from 'html-webpack-plugin'
+import path from 'path';
+import webpack from 'webpack';
+import HTMLWebpackPlugin from "html-webpack-plugin";
 
-
-module.exports = {
-  mode: 'development',
-  entry: path.resolve(__dirname, 'src', 'index.ts'),
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-  },
-  output: {
-    filename: '[name].[contenthash].js',
-    path: path.resolve(__dirname, 'build'),
-    clean: true,
-  },
-  plugins: [
-      new HtmlWebpackPlugin({
+const config: webpack.Configuration = {
+    mode: 'development',
+    entry: path.resolve(__dirname, 'src', 'index.ts'),
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+      filename: '[name].[contenthash].js',
+      path: path.resolve(__dirname, 'build'),
+      clean: true,
+    },
+    plugins: [
+      new HTMLWebpackPlugin({
         template: path.resolve(__dirname, 'public', 'index.html'),
         title: 'My App',
         filename: 'index.html'
       }),
       new webpack.ProgressPlugin()
-  ],
+    ],
 }
+
+export default config
