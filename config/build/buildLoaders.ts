e23cf868594da4ace71407dd7291c941cc9f924b
KDV <wexwexwex1@gmail.com>
2023-05-26 23:46:52 +0800

    enhanced scss (scss modules) , by adding MiniCssExtractPlugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,6 +1,8 @@
 import webpack from "webpack";
+import {BuildOptions} from "./types/config";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
 
-export function buildLoaders(): webpack.RuleSetRule[] {
+export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
   
   const typescriptLoader = {
     // if I don't want to use typescript instead using javascript , then should add babel-loader
@@ -12,8 +14,18 @@ export function buildLoaders(): webpack.RuleSetRule[] {
   const cssLoader = {
     test: /\.s[ac]ss$/i,
     use: [
-      "style-loader",
-      "css-loader",
+      isDev ? "style-loader" : MiniCssExtractPlugin.loader,
+      {
+        loader: "css-loader",
+        options: {
+          modules: {
+            auto: (resourcePath: string) => resourcePath.endsWith('.module.scss'),
+            localIdentName: isDev
+              ? '[path][name]__[local]'
+              : '[hash:base64:8]',
+          }
+          },
+      },
       "sass-loader"
     ],
   }
--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,6 +1,7 @@
 import HTMLWebpackPlugin from "html-webpack-plugin";
 import webpack from "webpack";
 import {BuildOptions} from "./types/config";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
 
 export function buildPlugins({paths}: BuildOptions ): webpack.WebpackPluginInstance[] {
   
@@ -10,7 +11,13 @@ export function buildPlugins({paths}: BuildOptions ): webpack.WebpackPluginInsta
       title: 'My App',
       filename: 'index.html'
     }),
-    new webpack.ProgressPlugin()
+    new webpack.ProgressPlugin(),
+    new MiniCssExtractPlugin(
+      {
+        filename: 'css/[name].[contenthash:8].css',
+        chunkFilename: 'css/[name].[contenthash:8].chunk.css',
+      }
+    )
   ]
   
 }
