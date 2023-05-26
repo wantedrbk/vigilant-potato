f9dcc73dfb02a228e97daa076d50a2049ef41260
KDV <wexwexwex1@gmail.com>
2023-05-26 19:23:25 +0800

    some dev server settings , added env using scripts

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -7,7 +7,7 @@ import {buildDevServer} from "./buildDevServer";
 
 export function buildWebpackConfig(options: BuildOptions): webpack.Configuration {
   
-  const {paths, mode} = options;
+  const {paths, mode, isDev} = options;
   
   return {
     mode: mode,
@@ -22,8 +22,8 @@ export function buildWebpackConfig(options: BuildOptions): webpack.Configuration
     module: {
       rules: buildLoaders()
     },
-    devtool: "inline-source-map",
-    devServer: buildDevServer(options),
+    devtool: isDev ? "inline-source-map" : false,
+    devServer: isDev ? buildDevServer(options) : undefined,
   }
   
 }
