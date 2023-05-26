d05fdc5b941cc370630cf38b7b3a4ff28059d387
KDV <wexwexwex1@gmail.com>
2023-05-26 21:01:29 +0800

    added scss preprocessor

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -3,13 +3,24 @@ import webpack from "webpack";
 export function buildLoaders(): webpack.RuleSetRule[] {
   
   const typescriptLoader = {
+    // if I don't want to use typescript instead using javascript , then should add babel-loader
     test: /\.tsx?$/,
     use: 'ts-loader',
     exclude: /node_modules/,
   }
   
+  const cssLoader = {
+    test: /\.s[ac]ss$/i,
+    use: [
+      "style-loader",
+      "css-loader",
+      "sass-loader"
+    ],
+  }
+  
   return [
-    typescriptLoader
+    typescriptLoader,
+    cssLoader,
   ]
   
 }
--- src/test.ts
+++
@@ -1,4 +0,0 @@
-
-export function someFn(arg:number) {
-  console.log(`someFn(${arg}) called one more time`);
-}
