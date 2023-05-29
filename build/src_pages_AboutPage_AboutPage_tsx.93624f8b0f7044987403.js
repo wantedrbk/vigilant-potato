53b822dde9c78363fc6c7cd3e613eca926aedb3d
KDV <wexwexwex1@gmail.com>
2023-05-29 21:16:25 +0700

    added router, react dom, and split for chunks

---
+++ build/src_pages_AboutPage_AboutPage_tsx.93624f8b0f7044987403.js
@@ -0,0 +1,25 @@
+"use strict";
+(self["webpackChunk_1"] = self["webpackChunk_1"] || []).push([["src_pages_AboutPage_AboutPage_tsx"],{
+
+/***/ "./src/pages/AboutPage/AboutPage.tsx":
+/*!*******************************************!*\
+  !*** ./src/pages/AboutPage/AboutPage.tsx ***!
+  \*******************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react/jsx-runtime */ "./node_modules/react/jsx-runtime.js");
+
+var AboutPage = function () {
+    return ((0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)("div", { children: "About Page" }));
+};
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (AboutPage);
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoic3JjX3BhZ2VzX0Fib3V0UGFnZV9BYm91dFBhZ2VfdHN4LjkzNjI0ZjhiMGY3MDQ0OTg3NDAzLmpzIiwibWFwcGluZ3MiOiI7Ozs7Ozs7Ozs7Ozs7O0FBQWdEO0FBQ2hEO0FBQ0EsWUFBWSxzREFBSSxVQUFVLHdCQUF3QjtBQUNsRDtBQUNBLGlFQUFlLFNBQVMsRUFBQyIsInNvdXJjZXMiOlsid2VicGFjazovLzEvLi9zcmMvcGFnZXMvQWJvdXRQYWdlL0Fib3V0UGFnZS50c3giXSwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IHsganN4IGFzIF9qc3ggfSBmcm9tIFwicmVhY3QvanN4LXJ1bnRpbWVcIjtcbnZhciBBYm91dFBhZ2UgPSBmdW5jdGlvbiAoKSB7XG4gICAgcmV0dXJuIChfanN4KFwiZGl2XCIsIHsgY2hpbGRyZW46IFwiQWJvdXQgUGFnZVwiIH0pKTtcbn07XG5leHBvcnQgZGVmYXVsdCBBYm91dFBhZ2U7XG4iXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, {Suspense} from 'react';
+import {Link, Route, Routes} from 'react-router-dom';
 import {Counter} from "./components/Counter";
 import './index.scss'
+import MainPage from "./pages/MainPage/MainPage";
+import AboutPage from "./pages/AboutPage/AboutPage";
+import {MainPageAsync} from "./pages/MainPage/MainPage.async";
+import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
 
 const App = () => {
   return (
     <div className="app">
-      <p>asda</p>
-      <Counter />
-    </div>
+      <Link to={'/'}>Главная страница</Link>
+      <Link to={'/about'}>О сайте</Link>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="/" element={<MainPageAsync />} />
+            <Route path="/about" element={<AboutPageAsync />}/>
+          </Routes>
+        </Suspense>
+       </div>
   );
 };
 
