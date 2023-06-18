c6ad1a9c99cc03ce4b83812dc6f08828c5ae1dbd
KDV <wexwexwex1@gmail.com>
2023-06-18 23:57:47 +0800

    created light/dark switcher v.1 with custom button

--- src/app/providers/ThemeProvider/lib/useTheme.ts
+++ src/app/providers/ThemeProvider/lib/useTheme.ts
@@ -7,7 +7,9 @@ interface useThemeResult {
 }
 
 export function useTheme(): useThemeResult {
+
   const {theme , setTheme} = useContext(ThemeContext)
+
   const toggleTheme = () => {
     const newTheme = theme === Theme.LIGHT ? Theme.DARK : Theme.LIGHT
     setTheme(newTheme)
