8c86e2db2607c6815f48fc820bebc44b277a239e
KDV <wexwexwex1@gmail.com>
2023-06-19 11:42:09 +0800

    added sidebar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,13 +4,17 @@ import {useTheme} from "app/providers/ThemeProvider";
 import {classNames} from "shared/lib/classNames/classNames";
 import {AppRouter} from "app/providers/router";
 import {NavBar} from "widgets/Navbar";
+import {Sidebar} from "widgets/SideBar";
 
 const App = () => {
   const {theme} = useTheme()
   return (
     <div className={classNames('app', {} , [theme])}>
         <NavBar />
-        <AppRouter />
+        <div className="content-page">
+          <Sidebar />
+          <AppRouter />
+        </div>
        </div>
   );
 };
--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -10,7 +10,13 @@ const AppRouter = () => {
                     <Route
                         key={path}
                         path={path}
-                        element={element}
+                        element={
+                          (
+                            <div className="page-wrapper">
+                              {element}
+                            </div>
+                          )
+                        }
                     />
                     ))}
             </Routes>
