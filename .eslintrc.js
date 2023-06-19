9036a4059936ed5527e9b0c474cb1f7a36201f65
KDV <wexwexwex1@gmail.com>
2023-06-19 23:27:05 +0800

    added stylelint , i18n

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,9 +4,10 @@ module.exports = {
         "es2021": true
     },
     "extends": [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:react/recommended"
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react/recommended',
+        'plugin:i18next/recommended'
     ],
     "overrides": [
         {
@@ -28,7 +29,8 @@ module.exports = {
     },
     "plugins": [
         "@typescript-eslint",
-        "react"
+        "react",
+        "i18next",
     ],
     "rules": {
         'react/jsx-indent': [2, 4],
@@ -38,5 +40,6 @@ module.exports = {
         'import/prefer-default-export': 'off',
         'no-unused-vars': 'off',
         'react/require-default-props': 'off',
+        'i18next/no-literal-string': ['error', {markupOnly: true}]
     }
 }
