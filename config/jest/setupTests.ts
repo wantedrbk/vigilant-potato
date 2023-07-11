d29796db899ae9070bd76005210168dc3a242170
Dmytro Sanchez <55120646+wantedrbk@users.noreply.github.com>
2023-07-11 18:55:41 +0800

    added checks (#12)
    * added checks
    
    * added checks(v2)
    
    * added checks(v3)
    
    * added checks(v4) fixed some errors
    
    * added checks(v4) fixed some errors
    
    * added checks(v6) fixed some errors
    
    * added checks(v7) fixed some errors
    
    * added checks(v7) fixed some errors
    
    * added checks(v8) fixed some errors
    
    * added checks(v9) fixed some errors
    
    * added checks(v10) fixed some errors
    
    * added checks(v11) fixed some errors
    
    * added checks(v12) fixed some errors

--- config/jest/setupTests.ts
+++ config/jest/setupTests.ts
@@ -1,2 +1,2 @@
 import '@testing-library/jest-dom'
-import 'generator-runtime/runtime'
+import 'regenerator-runtime/runtime';
--- src/widgets/SideBar/index.ts
+++ src/widgets/SideBar/index.ts
@@ -1,1 +1,1 @@
-export { Sidebar } from './ui/Sidebar/SideBar';
+export { Sidebar } from './ui/Sidebar/Sidebar';
